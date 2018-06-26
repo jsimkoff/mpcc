3d9b1296799ee27ce2df8714f742f785b28c8d36
jsimkoff <jsimkof@sandia.gov>
2018-06-26 17:27:23 -0600

    MPCC and bigM formulations implemented

--- tanks_GDP.py
+++ tanks_GDP.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 Ntank = 10
 m = ConcreteModel()
 m.I = RangeSet(Ntank)
-m.t = ContinuousSet(bounds=[0,100])
+m.t = ContinuousSet(bounds=[0,60])
 
 m.L = Var(m.I, m.t, within=NonNegativeReals, initialize=0.75)
 m.delL = Var(m.I, m.t, within=NonNegativeReals)
@@ -29,11 +29,11 @@ m.A = Param(m.I, initialize=1.0)
 
 m.dLdt = DerivativeVar(m.L, wrt=m.t)
 
-m.y1 = Var(m.I, m.t, within=NonNegativeReals)
-m.y2 = Var(m.I, m.t, within=NonNegativeReals)
+m.y1 = Var(m.I, m.t, within=Binary)
+m.y2 = Var(m.I, m.t, within=Binary)
+m.M1 = 0.6
+m.M2 = 0.4
 
-m.sp = Var(m.I, m.t, within=NonNegativeReals)
-m.sn = Var(m.I, m.t, within=NonNegativeReals)
 
 @m.Constraint(m.I, m.t)
 def _Ldot(m, i, t):
@@ -65,21 +65,18 @@ def _delL(m, i, t):
     else:
         return Constraint.Skip
 
-@m.Constraint(m.I, m.t)
-def _slacks(m, i, t):
-    return m.sp[i, t] - m.sn[i, t] == m.L[i, t] - m.H[i]
 
 @m.Constraint(m.I, m.t)
 def _ysum(m, i, t):
     return m.y1[i, t] + m.y2[i, t] == 1
 
-# @m.Constraint(m.I, m.t)
-# def compslack1(m, i, t):
-#     return  m.y2[i, t]*m.sp[i, t] <= 1e-1
-#
-# @m.Constraint(m.I, m.t)
-# def compslack2(m, i, t):
-#     return m.y1[i, t]*m.sn[i, t] <= 1e-1
+@m.Constraint(m.I, m.t)
+def _bigM1(m, i, t):
+    return -m.M1*(1 - m.y1[i, t]) <= m.L[i, t] - m.H[i]
+
+@m.Constraint(m.I, m.t)
+def _bigM2(m, i, t):
+    return m.L[i, t] - m.H[i] <= m.M2*(1 - m.y2[i, t])
 
 @m.ConstraintList()
 def ICs(m):
@@ -89,8 +86,8 @@ def ICs(m):
 
 discretizer = TransformationFactory('dae.collocation')
 discretizer.apply_to(m, nfe=20, ncp=1)
-# discretizer.reduce_collocation_points(m, var=m.w,ncp=1,contset=m.t)
-# add rate of change constraints to valve opening proble
+
+# add rate of change constraints to valve opening problem
 
 @m.ConstraintList()
 def RoC(m):
@@ -101,24 +98,25 @@ def RoC(m):
                 yield (m.w[i, tplus] - m.w[i,t])**2 <= 0.04
             yield (m.w0[tplus] -m.w0[t])**2 <= 0.04
             t = tplus
-#
-# m.obj_var = Var(initialize=0,within=NonNegativeReals)
-# @m.Constraint()
-# def _obj_var(m):
-#     m.obj_var = sum(sum((0.75-m.L[i,t])**2 for t in m.t) for i in m.I)
 
-m.obj = Objective(sense=minimize, expr=(sum(sum((0.75-m.L[i,t])**2 + m.y2[i,t]*m.sp[i,t] \
-                                                    + m.y1[i, t]*m.sn[i, t] for t in m.t) for i in m.I)\
-                                                    # +m.y1[1,20]*m.sn[1,20]*100+ m.y1[1,25]*m.sn[1,25]*100\  # had to tweak for 10 tank case
-                                                    # +m.y1[1,15]*m.sn[1,15]*100+ m.y1[1,30]*m.sn[1,30]*100     # - especially degenerate(?)
-                                                    ))
+# big-M formulation seems to need additional endpoint constraint
+
+# @m.Constraint(m.I)
+# def endpoint(m, i):
+#     tfinal = m.t.last()
+#     return inequality(0.749, m.L[i, tfinal], 0.751)
 
 
-solver = SolverFactory('ipopt')
-solver.options["ma27_pivtol"] = 1e-10
-solver.options["tol"] = 1e-6
+
+m.obj = Objective(sense=minimize, expr=sum(sum((0.75-m.L[i,t])**2 for t in m.t) for i in m.I))
+
+
+solver = SolverFactory('bonmin')
+solver.options["ma27_pivtol"] = 1e-6
+solver.options["tol"] = 1e-12
 results = solver.solve(m, tee=True)
 print("time: %0.4f\n" % results['Solver'][0]['Time'])
+
 m.obj.display()
 
 ######### PLOTS ##########
@@ -164,10 +162,12 @@ for t in m.t:
     l1.append(m.L[1,t].value)
     l2.append(m.L[2,t].value)
     l3.append(m.L[3,t].value)
+
     F0.append(m.F0[t].value)
     F1.append(m.F[1,t].value)
     F2.append(m.F[2,t].value)
     F3.append(m.F[3,t].value)
+
     w0.append(m.w0[t].value)
     w1.append(m.w[1,t].value)
     w2.append(m.w[2,t].value)
@@ -175,27 +175,21 @@ for t in m.t:
 
     y1_1.append(m.y1[1,t].value)
     y2_1.append(m.y2[1,t].value)
-    sp_1.append(m.sp[1,t].value)
-    sn_1.append(m.sn[1,t].value)
     y1_2.append(m.y1[2,t].value)
     y2_2.append(m.y2[2,t].value)
-    sp_2.append(m.sp[2,t].value)
-    sn_2.append(m.sn[2,t].value)
     y1_3.append(m.y1[3,t].value)
     y2_3.append(m.y2[3,t].value)
-    sp_3.append(m.sp[3,t].value)
-    sn_3.append(m.sn[3,t].value)
     if Ntank > 3:
         y1_4.append(m.y1[4,t].value)
         y2_4.append(m.y2[4,t].value)
         y1_5.append(m.y1[5,t].value)
         y2_5.append(m.y2[5,t].value)
-        l4.append(m.L[4,t].value)
-        l5.append(m.L[5,t].value)
         w4.append(m.w[4,t].value)
         w5.append(m.w[5,t].value)
         F4.append(m.F[4,t].value)
         F5.append(m.F[5,t].value)
+        l4.append(m.L[4,t].value)
+        l5.append(m.L[5,t].value)
 
 plt.subplot(221)
 
@@ -226,17 +220,11 @@ if Ntank > 3:
 plt.legend()
 plt.subplot(224)
 plt.plot(m.t, y2_1, label='y2_1')
-# plt.plot(m.t, sp_1, label='sp_1')
 plt.plot(m.t, y1_1, label='y1_1')
-# plt.plot(m.t, sn_1, label='sn_1')
 plt.plot(m.t, y2_2, label='y2_2')
-# plt.plot(m.t, sp_2, label='sp_2')
 plt.plot(m.t, y1_2, label='y1_2')
-# plt.plot(m.t, sn_2, label='sn_2')
 plt.plot(m.t, y2_3, label='y2_3')
-# plt.plot(m.t, sp_3, label='sp_3')
 plt.plot(m.t, y1_3, label='y1_3')
-# plt.plot(m.t, sn_3, label='sn_3')
 if Ntank > 3:
     plt.plot(m.t, y2_4, label='y2_4')
     plt.plot(m.t, y1_4, label='y1_4')
